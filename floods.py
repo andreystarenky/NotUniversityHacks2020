791987b8e1ee9c8280622607707e88f6c121f116
andreystarenky <47681471+andreystarenky@users.noreply.github.com>
2020-06-06 19:20:58 -0400

    more test data

--- floods.py
+++ floods.py
@@ -1,7 +1,10 @@
 import json
 import requests
 
+# DATA FOR WEATHER IN GIVEN LOCATION
 #weatherData = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&%20exclude=hourly,daily&appid=83231dc5b0b225eb97bb206cc1023ece")
+
+# GET WATER DATA FOR GIVEN STATION
 #waterData = requests.get("https://vps267042.vps.ovh.ca/scrapi/stations?page=7&key=-M99M-mwrZtTa9QBKKgx")
 #weatherData = requests.get()
 
