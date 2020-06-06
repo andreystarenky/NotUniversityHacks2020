2d705427ebd48b7f5e3fead2f3042485411ea495
andreystarenky <47681471+andreystarenky@users.noreply.github.com>
2020-06-06 19:12:36 -0400

    API_setup

---
+++ floods.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+#weatherData = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&%20exclude=hourly,daily&appid=83231dc5b0b225eb97bb206cc1023ece")
+#waterData = requests.get("https://vps267042.vps.ovh.ca/scrapi/stations?page=7&key=-M99M-mwrZtTa9QBKKgx")
+#weatherData = requests.get()
+
+with open("weather.json", 'r', ) as file:
+    weatherString = file.read()
+
+#print(weatherString)
+
+testData = json.loads(weatherString)
+
+print(testData["daily"])
