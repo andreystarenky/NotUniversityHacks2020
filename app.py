06d8065057e58f2a47ad85fe2734722e473a78fa
andreystarenky <47681471+andreystarenky@users.noreply.github.com>
2020-06-06 21:14:05 -0400

    Web Update - Flask Connected

--- app.py
+++ app.py
@@ -6,10 +6,25 @@ app = Flask(__name__,
             static_folder='black-dashboard-master',
             template_folder='black-dashboard-master\examples')
 
-@app.route('/')
-def hello():
+@app.route('/home')
+def dashboard():
     print("HELLO")
     return render_template("dashboard.html")
+
+@app.route('/map')
+def map():
+    print("HELLO")
+    return render_template("map.html")
+
+@app.route('/notifications')
+def notifications():
+    print("HELLO")
+    return render_template("notifications.html")
+
+@app.route('/tables')
+def tables():
+    print("HELLO")
+    return render_template("tables.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
